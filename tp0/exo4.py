ab943a5b68bd64533b4a32966b49f0e41ca22179
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4171-linux.ad.polytech.sorbonne-universite.fr>
2026-09-15 17:25:53 +0200

    exo4 questions 1 et 2

--- tp0/exo4.py
+++ tp0/exo4.py
@@ -1,16 +1,38 @@
-"""
-exo4
-"""
+robots_exploration = {"R2", "R5", "R7"}
+robots_transport = {"R5", "R9", "R7", "R3"}
 
-pieces_stock = {
-"ModeleA": {"moteurs": 10, "capteurs": 25, "roues": 40},
-"ModeleB": {"moteurs": 6, "capteurs": 15, "roues": 24},
-}
+def robots_double_mission(set1,set2) :
+    return set1 & set2
 
-def quantite_piece(stock, modele, piece) :
-    """
-    retourne nb piece
-    """
-    return stock[modele][piece]
+def robots_toutes_missions(set1,set2) :
+    return set1|set2
 
-assert quantite_piece(pieces_stock, "ModeleA", "moteurs") == 10
+def robots_exploration_seulement(set1,set2) :
+    return set1 - set2
+
+double_mission = robots_double_mission(
+robots_exploration, robots_transport)
+toutes_missions = robots_toutes_missions(
+robots_exploration, robots_transport)
+exploration_seule = robots_exploration_seulement(robots_exploration, robots_transport)
+assert double_mission == {"R5", "R7"}
+assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
+assert exploration_seule == {"R2"}
+
+def ajouter_robot_mission(set,elem) :
+    nouv = set.copy()
+    nouv.add(elem)
+    return nouv
+
+def retirer_robot_mission(set,elem) :
+    nouv = set.copy()
+    nouv.remove(elem)
+    return nouv
+
+
+ajout = ajouter_robot_mission(robots_exploration, "R8")
+retrait = retirer_robot_mission(robots_transport, "R9")
+assert ajout == {"R2", "R5", "R7", "R8"}
+assert retrait == {"R3", "R5", "R7"}
+# L’ensemble d’origine ne doit pas avoir été modifié
+assert robots_transport == {"R5", "R9", "R7", "R3"}
