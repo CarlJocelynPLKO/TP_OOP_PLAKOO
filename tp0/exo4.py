096c7bbbd1e364f77492dfa70a07d7a94cb86e10
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4171-linux.ad.polytech.sorbonne-universite.fr>
2026-09-15 16:19:06 +0200

    exo4 question 1

---
+++ tp0/exo4.py
@@ -0,0 +1,16 @@
+"""
+exo4
+"""
+
+pieces_stock = {
+"ModeleA": {"moteurs": 10, "capteurs": 25, "roues": 40},
+"ModeleB": {"moteurs": 6, "capteurs": 15, "roues": 24},
+}
+
+def quantite_piece(stock, modele, piece) :
+    """
+    retourne nb piece
+    """
+    return stock[modele][piece]
+
+assert quantite_piece(pieces_stock, "ModeleA", "moteurs") == 10
