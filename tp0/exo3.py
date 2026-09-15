37e0ede5c2ad5a2cc64db2a2d61afd7269894f65
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4171-linux.ad.polytech.sorbonne-universite.fr>
2026-09-15 15:35:14 +0200

    exo3 question2

--- tp0/exo3.py
+++ tp0/exo3.py
@@ -8,7 +8,23 @@ def afficher_releve(a) :
  
 
   return "Capteur " + a[0] + " : "+ str(a[1])+ " " + a[2]
-
+assert len(releves) == 3
+assert releves[0][0] == "laser_avant"
 assert afficher_releve(releve1) == "Capteur laser_avant : 2.35 m"
 
 
+def recalibrer(releve, capteur, mes ) : 
+   
+    
+    for i in range(0, len(releve)-1) :
+        if releve[i][0] == capteur :
+            unit = releve[i][2]
+            nouv = ( capteur, mes, unit)
+            releve[i] = nouv
+    
+
+    return releve
+nouveaux_releves = recalibrer(releves, "laser_avant", 2.40)
+assert nouveaux_releves[0] == ("laser_avant", 2.40, "m")
+assert nouveaux_releves[1] == releve2
+assert nouveaux_releves[2] == releve3
