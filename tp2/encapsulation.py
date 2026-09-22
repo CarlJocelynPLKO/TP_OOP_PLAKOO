78e77bb9c5c00115857facbf9596aa76be75e3d3
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4171-linux.ad.polytech.sorbonne-universite.fr>
2026-09-22 18:02:45 +0200

    exo4 question 1 & 2

--- tp2/encapsulation.py
+++ tp2/encapsulation.py
@@ -8,8 +8,6 @@ class Habitant() :
 
     def get_nom(self):
         return self.__nom
-    def get_age(self) :
-        return self.__age
     def get_adresse(self) :
         return self.__adresse
     def get_animaux(self) :
@@ -24,6 +22,15 @@ class Habitant() :
     def set_animaux(self,animaux):
         self.__animaux = animaux
 
+    @property
+    def age(self) :
+        return self.__age
+    @age.setter
+    def age(self,age) :
+        if ((age>130)|(age<0)):
+            raise ValueError
+        self.__age = age
+
     def affichage_adresse(self,nom,adresse) :    
         print(self.__nom + " habite a"+ self.__adresse)
     
@@ -32,5 +39,12 @@ class Habitant() :
             if anim == animal :
                 return self.__animaux[animal]
         return 0   
-    
-                
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+
+h1.age = 26
+assert h1.age == 26
+try:
+    h1.age = -5
+    assert False, "une ValueError aurait du etre levee"
+except ValueError:
+    pass
